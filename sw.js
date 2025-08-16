8e4c5702b96297550289e11fa45cef9938db82e7
juniorlaunther <40452159+juniorlaunther@users.noreply.github.com>
2025-08-16 16:24:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'ludo-juheb-v7';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if(req.method !== 'GET') return;
+  event.respondWith(
+    fetch(req).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(req, clone));
+      return res;
+    }).catch(()=> caches.match(req).then(cached => cached || caches.match('./index.html')))
+  );
+});
